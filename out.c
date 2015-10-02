f8ead185b7b215c056f752de8fb37702780fd84b
Joe Pelz <joe.pelz@gmail.com>
2015-10-02 15:00:35 -0700

    Updated tests and removed explict types test

--- out.c
+++ out.c
@@ -1,18 +1,38 @@
 #include "rsl.h"
 #include "out.h"
 int main(void) {
-include "rsl.h"
-include "99bottles.h"
-	Identifier_main_???(int,void); 
-	Identifier_main_???(int,void); 
-	???_b(int); 
-	???_type(ob); 
-	???_type(line); 
-	int b;
-	Identifier_assign_void(line,void_type((Identifier_type_void(plus,Integer_bottles((UNKNOWNTYPE Integer UNKNOWNOBJECT)))))); 
-	void_while((Identifier_type_Boolean(UNKNOWNTYPE,UNKNOWNOBJECT))); 
-	void_while((Identifier_type_Boolean(UNKNOWNTYPE,UNKNOWNOBJECT))); 
-	???_type(w); 
-		void_print((Identifier_type_void(Stream,stdout))); 
+//Type inferencing
+	String hello;
+	hello = String_stringlit("Hello"); 
+	String world;
+	world = String_stringlit("World"); 
+//Way better string handling
+	String helloWorld;
+	helloWorld = (String_plus_String(hello,(String_plus_String(String_stringlit(" "),world)))); 
+//Semicolons are optional
+	Stream_print_String(stdout,helloWorld); 
+	int n;
+	n = 5; 
+//pow is now ^
+	float f2;
+	float f1;
+	float f;
+	f = (f1 = (f2 = (Float_exponent_Integer(5.5,2)))); 
+//echo does not print newline
+	Stream_echo_String(stdout,(String_plus_Float(String_stringlit("f2 is and "),f2))); 
+	float f3;
+	f3 = (Integer_exponent_Integer((((2 * 3) + 1) * 4),3)); 
+//print does
+	Stream_print_String(stdout,(String_plus_Float(String_stringlit(" and f3 is "),f3))); 
+//No curly braces (if is not actually a keyword)
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(1 > (n / f))){
+//End code block with .
+		Stream_print_String(stdout,(String_plus_Float(String_stringlit("1 is more than "),(n / f))));}
+//Nicer for loops, (for is not a keyword)
+	int i;
+	Identifier_for_Integer_Integer(i,1,(n)){
+		Stream_print_Integer(stdout,i);}
+//You can embed C code 
+printf ("This is in C :) \n");
 	return 0;
 }
--- tests/out.c
+++ tests/out.c
@@ -1,19 +1,83 @@
 #include "rsl.h"
 #include "out.h"
 int main(void) {
-	String a;
-	a = String_stringlit("hello"); 
-//TODO: not sure if this is supposed to work or not. Right now it doesn't
-//String b = " "
-	String b;
-	b = String_stringlit(" "); 
-	String c;
-	c = String_stringlit("world"); 
-	String d;
-	d = (String_plus_String(a,c)); 
-	String e;
-	e = b; 
-	Stream_print_String(stdout,(String_plus_String(a,(String_plus_String(b,c))))); 
-	Stream_print_String(stdout,(String_plus_String(d,(String_plus_String(String_stringlit("|"),(String_plus_String(e,String_stringlit("|")))))))); 
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(1 > 2)){
+		Stream_print_String(stdout,String_stringlit("1 > 2 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("1 > 2 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(2 > 1)){
+		Stream_print_String(stdout,String_stringlit("2 > 1 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("2 > 1 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(1 < 2)){
+		Stream_print_String(stdout,String_stringlit("1 < 2 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("1 < 2 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(2 < 1)){
+		Stream_print_String(stdout,String_stringlit("2 < 1 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("2 < 1 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(3 == 2)){
+		Stream_print_String(stdout,String_stringlit("3 == 2 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("3 == 2 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(3 == 3)){
+		Stream_print_String(stdout,String_stringlit("3 == 3 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("3 == 3 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(1.0 > 2.0)){
+		Stream_print_String(stdout,String_stringlit("1.0 > 2.0 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("1.0 > 2.0 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(2.0 > 1.0)){
+		Stream_print_String(stdout,String_stringlit("2.0 > 1.0 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("2.0 > 1.0 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(1.0 < 2.0)){
+		Stream_print_String(stdout,String_stringlit("1.0 < 2.0 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("1.0 < 2.0 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(2.0 < 1.0)){
+		Stream_print_String(stdout,String_stringlit("2.0 < 1.0 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("2.0 < 1.0 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(3.0 == 2.0)){
+		Stream_print_String(stdout,String_stringlit("3.0 == 2.0 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("3.0 == 2.0 = False")); 
+	}
+	UNKNOWNTYPE_if_Boolean(UNKNOWNOBJECT,(3.0 == 3.0)){
+		Stream_print_String(stdout,String_stringlit("3.0 == 3.0 = True")); 
+	}
+	UNKNOWNTYPE_else(UNKNOWNOBJECT){
+		Stream_print_String(stdout,String_stringlit("3.0 == 3.0 = False")); 
+	}
+	Stream_print_String(stdout,(String_plus_String(String_stringlit("1<>2 = "),(Ternary_pick_String_String_String((Integer_compare_Integer(1,2)),String_stringlit("Greater"),(String_stringlit("Lesser")),(String_stringlit("Equal"))))))); 
+	Stream_print_String(stdout,(String_plus_String(String_stringlit("1<>1 = "),(Ternary_pick_String_String_String((Integer_compare_Integer(1,1)),String_stringlit("Greater"),(String_stringlit("Lesser")),(String_stringlit("Equal"))))))); 
+	Stream_print_String(stdout,(String_plus_String(String_stringlit("1<>0 = "),(Ternary_pick_String_String_String((Integer_compare_Integer(1,0)),String_stringlit("Greater"),(String_stringlit("Lesser")),(String_stringlit("Equal"))))))); 
+	Stream_print_String(stdout,(String_plus_String(String_stringlit("1.0<>2.0 = "),(Ternary_pick_String_String_String((Integer_compare_Integer(1,2)),String_stringlit("Greater"),(String_stringlit("Lesser")),(String_stringlit("Equal"))))))); 
+	Stream_print_String(stdout,(String_plus_String(String_stringlit("1.0<>1.0 = "),(Ternary_pick_String_String_String((Integer_compare_Integer(1,1)),String_stringlit("Greater"),(String_stringlit("Lesser")),(String_stringlit("Equal"))))))); 
+	Stream_print_String(stdout,(String_plus_String(String_stringlit("1.0<>0.0 = "),(Ternary_pick_String_String_String((Integer_compare_Integer(1,0)),String_stringlit("Greater"),(String_stringlit("Lesser")),(String_stringlit("Equal"))))))); 
 	return 0;
 }
