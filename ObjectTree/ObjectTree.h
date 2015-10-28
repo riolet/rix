830d14331b95d68bf4d4aa60d3e157041229805c
Joe Pelz <joe.pelz@gmail.com>
2015-10-28 14:42:32 -0700

    Designed an object tree

--- ObjectTree/ObjectTree.h
+++ ObjectTree/ObjectTree.h
@@ -1,39 +1,39 @@
 typedef enum {
-	Undefined,
-	Variable,
-	Type,
-	Constructor,
-	Function,
-	CodeBlock
+  Undefined,
+  Variable,
+  Type,
+  Constructor,
+  Function,
+  CodeBlock
 } TYPE;
 
-typedef struct _Object			Object;
-typedef struct _ListObject	ListObject;
-typedef struct _ListString	ListString;
-typedef struct _ListType		ListType;
+typedef struct _Object      Object;
+typedef struct _ListObject  ListObject;
+typedef struct _ListString  ListString;
+typedef struct _ListType    ListType;
 
 struct _ListString {
-	char*				value;
-	ListString* next;
+  char*        value;
+  ListString*  next;
 };
 struct _ListType{
-	TYPE				value;
-	ListType*		next;
+  TYPE         value;
+  ListType*    next;
 };
 struct _ListObject{
-	Object*			value;
-	ListObject*	next;
+  Object*      value;
+  ListObject*  next;
 };
 
 struct _Object{
-	char*				name;						//symbol name     ("myInteger", "calcTotalArea ", "Rectangle")
-	char*				fullName;       //symbol fullname ("myInteger", "Integer_calcTotalArea_Rectangle_Rectangle", "BaseType_Rectangle")
-	Object*			parentScope;    //parent scope    (global scope, global scope, BaseType)
-	TYPE				type;           //What is this?   (Variable, Function, Class)
-	char*				returnType;     //What value type?(Integer,  Integer,  NULL)
-	ListType*		paramTypes;     //parameters?     (NULL,     [Integer, Integer], NULL)
-	ListObject*	definedSymbols; //Things inside?  (NULL, [Rectangle "r1", Rectangle "r2", Integer "a1", Integer "a2"], [Integer "w", Integer "h", Constructor "Rectangle", Function "Area"])
-	ListString*	code;           //CodeBlock       (NULL, "Integer ...calcTotalArea...(...) {...", "typedef struct...")
+  char*        name;           //symbol name     ("myInteger", "calcTotalArea ", "Rectangle")
+  char*        fullName;       //symbol fullname ("myInteger", "Integer_calcTotalArea_Rectangle_Rectangle", "BaseType_Rectangle")
+  Object*      parentScope;    //parent scope    (global scope, global scope, BaseType)
+  TYPE         type;           //What is this?   (Variable, Function, Class)
+  char*        returnType;     //What value type?(Integer,  Integer,  NULL)
+  ListType*    paramTypes;     //parameters?     (NULL,     [Integer, Integer], NULL)
+  ListObject*  definedSymbols; //Things inside?  (NULL, [Rectangle "r1", Rectangle "r2", Integer "a1", Integer "a2"], [Integer "w", Integer "h", Constructor "Rectangle", Function "Area"])
+  ListString*  code;           //CodeBlock       (NULL, "Integer ...calcTotalArea...(...) {...", "typedef struct...")
 };
 
 //mallocs memory and returns a pointer to a new Object
