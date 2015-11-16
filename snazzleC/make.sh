5a75e75ba552847507e077d99c769e3889b191b7
Joe Pelz <joe.pelz@gmail.com>
2015-11-15 18:00:03 -0800

    removed snazzleC

--- snazzleC/make.sh
+++
@@ -1,9 +0,0 @@
-bash clr.sh
-echo Clear complete.
-bash comp.sh
-echo -----------------------------------------------------------------
-echo beginning compilation of snazzle output file
-echo -----------------------------------------------------------------
-gcc snazzle.tab.c lex.yy.c -lfl -o snazzle
-echo Executing ./snazzle
-./snazzle
