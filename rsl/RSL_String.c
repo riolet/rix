193f5e7420ada1f89d11839570f914035336d060
Rohana Rezel <rohana.rezel@riolet.com>
2015-12-20 14:03:17 -0800

    Tests for Simplify adding external C libraries

---
+++ rsl/RSL_String.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "rsl/RSL_String.h"
+
+void String_return_GCC(String *s)
+{
+    s->isStored=2;
+}
+
+void String_cleanUp_GCC(String *s)
+{
+    if (!s->isStored==StringStatusLiteral) {
+        if (!s->isStored==StringStatusReturned) {
+            free(s->buffer);
+        } else {
+            s->isStored=StringStatusDelete;
+        }
+    }
+}
+
+void String_cleanUp(String *s)
+{
+    if (s->isStored==StringStatusDelete) {
+        free(s->buffer);
+    }
+}
+
+String String_$_stringlit(char *strlit)
+{
+    String s;
+    s.buffer = strlit;
+    s.cap = s.length = strlen(strlit);
+    s.isStored = StringStatusLiteral;
+    return s;
+}
+
+String String_$_assign_$_String(String *left, String right)
+{
+    left->length = right.length + 1;
+    left->buffer = malloc(left->length);
+    left->isStored = StringStatusStored;
+    memcpy(left->buffer, right.buffer, right.length);
+    return *left;
+}
+
+String String_$_plus_$_String(String left, String right)
+{
+    String newString;
+    newString.isStored = StringStatusDelete;
+    newString.buffer = malloc(left.length + right.length + 1);
+    memcpy(newString.buffer, left.buffer, left.length);
+    memcpy(newString.buffer + left.length, right.buffer, right.length);
+    newString.length = left.length + right.length;
+    newString.buffer[newString.length] = 0;
+    String_cleanUp(&left);
+    String_cleanUp(&right);
+    return newString;
+}
+
+String String_$_plus_$_Integer(String left, int right)
+{
+    String newString;
+    newString.isStored = StringStatusDelete;
+    char rightStr[RSL_STRING_MAX_BUFFLEN];
+    int right_length = snprintf(rightStr, RSL_STRING_MAX_BUFFLEN, "%i", right);
+    newString.buffer = malloc(left.length + right_length + 1);
+    memcpy(newString.buffer, left.buffer, left.length);
+    memcpy(newString.buffer + left.length, rightStr, right_length);
+    newString.length = left.length + right_length;
+    newString.buffer[newString.length] = 0;
+    String_cleanUp(&left);
+    return newString;
+}
+
+String Integer_$_plus_$_String(int left, String right)
+{
+    String newString;
+    newString.isStored = StringStatusDelete;
+    char leftStr[RSL_STRING_MAX_BUFFLEN];
+    int left_length = snprintf(leftStr, RSL_STRING_MAX_BUFFLEN, "%i", left);
+    newString.buffer = malloc(right.length + left_length + 1);
+    memcpy(newString.buffer, leftStr, left_length);
+    memcpy(newString.buffer + left_length, right.buffer, right.length);
+    newString.length = right.length + left_length;
+    newString.buffer[newString.length] = 0;
+    String_cleanUp(&right);
+    return newString;
+}
+
+String String_$_plus_$_Float(String left, float right)
+{
+    String newString;
+    newString.isStored = StringStatusDelete;
+    char rightStr[RSL_STRING_MAX_BUFFLEN];
+    int right_length = snprintf(rightStr, RSL_STRING_MAX_BUFFLEN, "%f", right);
+    newString.buffer = malloc(left.length + right_length + 1);
+    memcpy(newString.buffer, left.buffer, left.length);
+    memcpy(newString.buffer + left.length, rightStr, right_length);
+    newString.length = left.length + right_length;
+    newString.buffer[newString.length] = 0;
+    String_cleanUp(&left);
+    return newString;
+}
+
+String Float_$_plus_$_String(float left, String right)
+{
+    String newString;
+    newString.isStored = StringStatusDelete;
+    char leftStr[RSL_STRING_MAX_BUFFLEN];
+    int left_length = snprintf(leftStr, RSL_STRING_MAX_BUFFLEN, "%f", left);
+    newString.buffer = malloc(right.length + left_length + 1);
+    memcpy(newString.buffer + left_length, right.buffer, right.length);
+    memcpy(newString.buffer, leftStr, left_length);
+    newString.length = right.length + left_length;
+    newString.buffer[newString.length] = 0;
+    String_cleanUp(&right);
+    return newString;
+}
